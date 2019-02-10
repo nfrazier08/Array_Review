0ec6421f88a59620167954795f37f8dc7d9901a1
neliza <nicolasaelf64@gmail.com>
2019-02-10 06:29:54 -0700

    added people and medium arrays

---
+++ script.js
@@ -0,0 +1,15 @@
+const inspirationalWomen = [
+    {first: 'Sallie', last: 'Krawcheck', company: 'Ellevest', founded: 2014}, 
+    {first: 'Kathryn', last: 'Minshew' , company: 'The Muse', founded: 2011}, 
+    {first: 'Emilie', last: 'Aries', company: 'BossedUp', founded: 2015}, 
+    {first: 'Aminatou', last: 'Sow', company: 'Tech LadyMafia', founded: 2011}, 
+    {first: 'Whitney', last: 'White', company: 'Melanin Haircare', founded: 2015}, 
+    {first: 'Camille', last: 'Hearst', company: 'Kit', founded: 2015}, 
+    {first: 'Lauren', last: 'Hazzouri', company: 'HeyLauren', founded: 2016}
+    {first: 'Tiffany', last: 'Dufu', company: 'The Cru', founded: 2018}
+];
+
+const people = ['Obama, Michelle', 'Mai, Ella', 'Winfrey, Oprah', 'Dufu, Tiffany', 'St. John, Bozoma'];
+
+
+const medium = ['book', 'essay', 'article', 'video', 'book', 'essay', 'essay', 'book', 'video', 'article', 'article', 'book', 'book'];
